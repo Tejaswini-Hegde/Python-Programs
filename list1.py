4ac42f8f383ebb462cae9457a170eeafc8170eb7
Tejaswini Hegde <77977903+Tejaswini-Hegde@users.noreply.github.com>
2021-02-26 22:22:03 +0530

    list1
    This program describes basic  operations on list

---
+++ list1.py
@@ -0,0 +1,11 @@
+l1=[]
+n=int(input("Enter number of values"))
+print("Enter values\n")
+for i in range(0,n):
+    l1.append(input())
+print(l1)
+print("Enter a sum")
+for i in l1:
+    
+    if index(i)+index(
+    
